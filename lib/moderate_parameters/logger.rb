85b7932539a12dd9c639efaac84fc781de262b8b
Kyle Boe <ksboe1@gmail.com>
2019-04-10 15:52:34 -0700

    use relative paths

--- lib/moderate_parameters/logger.rb
+++ lib/moderate_parameters/logger.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-file = File.open("#{Rails.root}/log/moderate_parameters.log", File::WRONLY | File::APPEND)
-$moderate_parameters_logger = Logger.new(file)
+
+$moderate_parameters_logger = Logger.new('log/moderate_parameters.log')
 
 ActiveSupport::Notifications.subscribe('moderate_parameters') do |name, start, finish, id, payload|
   $moderate_parameters_logger.info "#{payload[:controller]}##{payload[:action]} #{payload[:message]}"
