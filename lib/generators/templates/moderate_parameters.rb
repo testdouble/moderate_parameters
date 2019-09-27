bc0450b60e78663aa8896ac4e6f6d701d8819b38
Kyle Boe <ksboe1@gmail.com>
2019-09-27 13:12:48 -0700

    add configuration for breadcrumb reads/writes

--- lib/generators/templates/moderate_parameters.rb
+++ lib/generators/templates/moderate_parameters.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 ModerateParameters.configure do |config|
-  config.breadcrumbs_enabled = false
+  # Enables/Disables logging occurrences of
+  # reading from ActionController::Parameters.
+  config.breadcrumb_reads_enabled = false
+
+  # Enables/Disables logging occurrences of
+  # writing to ActionController::Parameters.
+  config.breadcrumb_writes_enabled = false
 end
