5319bef0500d397e72083e3ded6ff9c639e41b39
Kyle Boe <ksboe1@gmail.com>
2019-10-18 12:31:35 -0700

    specify active support logger

--- lib/moderate_parameters/logger.rb
+++ lib/moderate_parameters/logger.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$moderate_parameters_logger = Logger.new('log/moderate_parameters.log')
+$moderate_parameters_logger = ActiveSupport::Logger.new('log/moderate_parameters.log')
 
 ActiveSupport::Notifications.subscribe('moderate_parameters') do |_, _, _, _, payload|
   $moderate_parameters_logger.info "#{payload[:controller]}##{payload[:action]} #{payload[:message]}"
