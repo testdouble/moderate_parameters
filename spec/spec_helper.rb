8ab53a99649e0aa22822585f860999d20bae26fb
nvick <nate.vick@hint.io>
2020-02-14 20:49:36 -0800

    move simplecov config to top

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter 'spec/'
+end
+
 require 'bundler/setup'
 require 'pry'
 require 'moderate_parameters'
-require 'simplecov'
-
-SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
